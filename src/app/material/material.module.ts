5693a644bbff497c0d9612ba5821daab5675fd1d
Repeynik <m16072005m@gmail.com>
2021-07-27 13:03:00 +0700

    ng

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,23 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+export { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...modules
+  ],
+  exports: [
+    ...modules
+  ]
+})
