c13f7a0f24a12fc4f4d9dce194907e207a172a31
Repeynik <m16072005m@gmail.com>
2021-08-04 14:19:55 +0700

    коммит

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
 
 
 import { Component, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
-
+import { FormControl, FormGroup } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./main.component.html",
@@ -13,19 +13,20 @@ export class MainComponent implements AfterViewInit {
 
   map: google.maps.Map ;
 
-  myLatlng = new google.maps.LatLng(-40.397, 150.644);
+  myLatlng = new google.maps.LatLng(51.5,-0.11);
 
   mapOptions = {
     zoom: 8,
-    center: this.myLatlng,
-    mapTypeId: 'satellite'
+    center: this.myLatlng
   };
 
   marker = new google.maps.Marker({
     position: this.myLatlng,
     icon: 'http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png',
-    title: 'место'
+    title: 'Лондон'
   });
+
+  transitLayer = new google.maps.TransitLayer();
 
 
   ngAfterViewInit(): void {
@@ -38,14 +39,27 @@ export class MainComponent implements AfterViewInit {
 
     this.marker.addListener('click', () => {
       let info = new google.maps.InfoWindow({
-        content: ' <h3>место в океане</h3><p>описание места</p>'
+        content: ' <h3>Лондон</h3><p>описание места</p>'
       })
       info.open(this.map, this.marker)
     });
 
     this.marker.setMap(this.map);
+
+    this.transitLayer.setMap(this.map);
   }
 
+  checkoutForm : FormGroup = new FormGroup({
+    userLAT: new FormControl('51.5'),
+    userLNG: new FormControl('-0.11')
+  })
+
+  onSubmit(): void{
+    this.checkoutForm.valueChanges.subscribe(changes =>
+      this.map.setCenter(new google.maps.LatLng(changes['userLAT'], changes['userLNG']))
+      );
+
+  }
 
 
 
