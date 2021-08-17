1438a15af9241671c583a51cf1808690e5e86dc2
Repeynik <m16072005m@gmail.com>
2021-08-17 10:57:15 +0700

    добавление геокода

--- src/app/main/geocode.service.ts
+++ src/app/main/geocode.service.ts
@@ -16,11 +16,7 @@ export class GeocodeService {
     return this.http.get(`https://api.opencagedata.com/geocode/v1/json?q=${latLong[0]}+${latLong[1]}&key=0efacb983fa94d74b497e78cebabd3f5`) as Observable<IGeoLoc>
   }
 
-  // getGeocode(latLong: number[][]): Observable<IGeoLoc> {
-  //   let b = `https://api.opencagedata.com/geocode/v1/json?q=${latLong[0]},${latLong[1]}&pretty=1&key=1a2c33ad8cb344148aa8908876097adb`
-  //   let a = this.http.get(b)
-  //   return this.http.get(b) as Observable<IGeoLoc>
-  // }
+
 }
 
 export interface IGeoLoc {
@@ -28,12 +24,12 @@ export interface IGeoLoc {
   licenses: any;
   rate: any;
   results: {
-    annotations: { DMS: { lat: "54° 0' 9.89172'' N", lng: "81° 59' 58.10028'' E" }, MGRS: "44UNE6550984289" };
-    bounds: { northeast: { lat: 54.012343, lng: 82.046137 }, southwest: { lat: 53.992265, lng: 81.7840879 } };
-    components: { "ISO_3166-1_alpha-2": "RU", "ISO_3166-1_alpha-3": "RUS", _category: "road", _type: "road" };
-    confidence: 6;
-    formatted: "unnamed road, Шайдурово, Новосибирская область, Россия"
-    geometry: { lat: 54.0027477, lng: 81.9994723 }
+    annotations: { DMS: { lat: string, lng: string }, MGRS: string };
+    bounds: { northeast: { lat: number, lng: number }, southwest: { lat: number, lng: number } };
+    components: { "ISO_3166-1_alpha-2": "RU" | "EN", "ISO_3166-1_alpha-3": "RUS" | "ENG", _category: string, _type: string,  city: string, city_district: string, continent: string, country: string, country_code: string, house_number: string, neighbourhood: string, political_union: string, postcode: string, road: string, state: string, suburb: string};
+    confidence: number;
+    formatted: string;
+    geometry: { lat: number, lng: number }
   }[];
   status: { code: number, message: string };
 }
